d03ca9feb432f5a54920cc5e5eeaffaf6a495633
Yash Kirar | Backend Dev <thisisyash76@gmail.com>
2026-02-19 18:08:14 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,53 @@
+from crud import create_note, get_all_notes, update_note, delete_note
+from models import NoteCreate, NoteUpdate
 from fastapi import FastAPI
-from app.models import Note
-import app.crud as crud
+
 
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"message":"welcome to YashTech Industries",
-            "message2":"Server is running!"}
+async def root():
+    return {"message": "server is running"}
 
 
-@app.post("/notes")
-def create_note_route(note:Note):
-    return crud.create_note(note.title,note.content)
 
 @app.get("/notes")
-def show_note():
-    return crud.get_note()
+async def all_notes():
+    return get_all_notes()
+
+
+
+@app.post("/note")
+async def add_note(note: NoteCreate):
+    note_id = create_note(note.title, note.content, note.category)
+    return {
+        "response": "note created successfully",
+        "note": {
+            "id": note_id,
+            "title": note.title,
+            "content": note.content,
+            "category": note.category
+        }
+    }
+
+#update route
+@app.put("/note/{note_id}")
+async def update_existing_note(note_id: int, note: NoteUpdate):
+    result = update_note(note_id, note.title, note.content, note.category)
+
+    if result == 0:
+        return {"error": "Note not found"}
+
+    return {"message": "Note updated successfully"}
+
+
+#delete route
+@app.delete("/note/{note_id}")
+async def delete_existing_note(note_id: int):
+    result = delete_note(note_id)
+
+    if result == 0:
+        return {"error": "Note not found"}
+
+    return {"message": "Note deleted successfully"}
+
