03da0b2a7059c227104c8a1970ae350afd76f0e8
Yash Kirar | Backend Dev <thisisyash76@gmail.com>
2026-02-19 18:07:54 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,19 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="notesdb"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
+
+if __name__ == "__main__":
+    conn = get_connection()
+    print("Connected successfully!")
+    conn.close()
+
